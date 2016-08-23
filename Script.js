a86103ec30b84142f65d86e47eb2540c1cfbac2b
bernieb78 <bernard@dreamscape-design.co.uk>
2016-08-23 13:42:11 +0100

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,15 @@
+<script type="text/javascript">
+//<![CDATA[
+var theForm = document.forms['form'];
+if (!theForm) {
+    theForm = document.form;
+}
+function __doPostBack(eventTarget, eventArgument) {
+    if (!theForm.onsubmit || (theForm.onsubmit() != false)) {
+        theForm.__EVENTTARGET.value = eventTarget;
+        theForm.__EVENTARGUMENT.value = eventArgument;
+        theForm.submit();
+    }
+}
+//]]>
+</script>
